598b437d68e3d3f6ce5721be69ab9faf96ce3fcf
nobody <az8321550@gmail.com>
2015-12-10 20:40:55 +0800

    first

---
+++ zh/js/layout.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+		$('#slides').slides({
+			preload: true,
+			preloadImage: 'images/loading.gif',
+			play: 3000,
+			pause: 1000,
+			hoverPause: true
+		});
+		
+		$('#select_lang').change(function(){ 
+			val = $(this).find(":selected").val();
+			Str = val;	//http://117.0.0.1/CPG/en/xxx.html
+			window.location.href= Str; 			
+		});
+		
+		
+});
